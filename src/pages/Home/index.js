181b051636905ba246a9f5871e5224ef628c6d49
Wesley Alves <wesley.alvesxl@gmail.com>
2020-09-08 20:59:09 -0300

    global styles added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Container } from './style';
+
 const Home = () => (
-    <h1>Welcome</h1>
+
+    <Container>
+        <h1>Welcome</h1>
+        <h1>to</h1>
+        <h1>React App</h1>
+    </Container>
+
 );
 
 export default Home;
